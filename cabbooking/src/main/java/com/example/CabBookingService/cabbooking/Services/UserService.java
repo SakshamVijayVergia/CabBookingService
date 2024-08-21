545da6f64a57991eb08462c64074bc4fc5114532
Saksham Vijayvergia <sakshamvijayvergia@gmail.com>
2024-08-21 18:17:21 +0530

    User Service

--- cabbooking/src/main/java/com/example/CabBookingService/cabbooking/Services/UserService.java
+++ cabbooking/src/main/java/com/example/CabBookingService/cabbooking/Services/UserService.java
@@ -10,9 +10,8 @@ import java.util.List;
 
 @Service
 public class UserService {
-    // Method to get the list of all users // can be used for future requirements
     private final List<User> users = new ArrayList<>();
-    // Method to add a new user
+
     public void addUser(User user) {
         users.add(user);
         System.out.println("User added: " + user.getName());
