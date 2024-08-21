aad4ee31f8181e346d53f4b2c917dc32252dbbe5
Saksham Vijayvergia <sakshamvijayvergia@gmail.com>
2024-08-21 18:06:57 +0530

    CabBookingApplication

--- cabbooking/src/main/java/com/example/CabBookingService/cabbooking/Services/UserService.java
+++ cabbooking/src/main/java/com/example/CabBookingService/cabbooking/Services/UserService.java
@@ -2,24 +2,20 @@ package com.example.CabBookingService.cabbooking.Services;
 
 import com.example.CabBookingService.cabbooking.Models.User;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Service
 public class UserService {
+    // Method to get the list of all users // can be used for future requirements
     private final List<User> users = new ArrayList<>();
     // Method to add a new user
     public void addUser(User user) {
         users.add(user);
         System.out.println("User added: " + user.getName());
-    }
-
-    // Method to get the list of all users // can be used for future requirements
-    public List<User> getUsers() {
-        return users;
     }
 
 }
